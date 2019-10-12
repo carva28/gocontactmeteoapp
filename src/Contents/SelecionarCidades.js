a14778cac863585913adb8d2c1318309edadf1b1
carva28 <carvalho_diogo@hotmail.com>
2019-10-12 13:28:26 +0100

    data autofill

--- src/Contents/SelecionarCidades.js
+++ src/Contents/SelecionarCidades.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
-import { Button, ButtonGroup, Table } from 'reactstrap';
+import { Button, ButtonGroup, Table, InputGroup, InputGroupAddon, Input, Card, CardImg, CardBody, Container, Row, Col } from 'reactstrap';
 import { Bar, Line, Pie } from 'react-chartjs-2';
+import Autocomplete from 'react-autocomplete';
 
 export default class Cidades extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      value: "",
       name: '',
       test1: '',
       test2: '',
@@ -25,17 +27,90 @@ export default class Cidades extends Component {
       labelCidade: null,
       sunsethour: null,
       sunrisehour: null,
-      tempoCity:null
+      tempoCity: null,
+      autocompleteData: []
+
     }
+    this.onChange = this.onChange.bind(this);
+    this.onSelect = this.onSelect.bind(this);
+    this.getItemValue = this.getItemValue.bind(this);
+    this.renderItem = this.renderItem.bind(this);
+    this.retrieveDataAsynchronously = this.retrieveDataAsynchronously.bind(this);
+
   }
 
+  retrieveDataAsynchronously(searchText) {
+    let _this = this;
 
-  static defaultProps = {
-    displayTitle: true,
-    displayLegend: true,
-    legendPosition: 'right',
-    location: 'City'
+    // Url of your website that process the data and returns a
+    let url = `http://localhost:8080/api/distritos`;
+
+    // Configure a basic AJAX request to your server side API
+    // that returns the data according to the sent text
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = () => {
+      let status = xhr.status;
+
+      if (status == 200) {
+        // Show response of your server in the console
+        for(let i=0;i<xhr.response[0].dados.cidades.length;i++){
+          if(searchText.charAt(0)===xhr.response[0].dados.cidades[i].geodsg.charAt()){
+            console.log(xhr.response[0].dados.cidades[i].geodsg);
+            if(searchText.includes(xhr.response[0].dados.cidades[i].geodsg)){
+              _this.setState({
+                autocompleteData: xhr.response
+              });
+            }
+          }
+
+        }
+        
+      } else {
+        console.error("Cannot load data from remote source");
+      }
+    };
+
+    xhr.send();
   }
+
+  onChange(e) {
+    this.setState({
+      value: e.target.value
+    });
+    this.retrieveDataAsynchronously(e.target.value);
+    console.log("The Input Text has changed to ", e.target.value);
+  }
+  onSelect(val) {
+    this.setState({
+      value: val
+    });
+
+    console.log("Option from 'database' selected : ", val);
+  }
+
+  renderItem(item, isHighlighted) {
+    return (
+      <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+        {item.label}
+      </div>
+    );
+  }
+
+  getItemValue(item) {
+    // You can obviously only return the Label or the component you need to show
+    // In this case we are going to show the value and the label that shows in the input
+    // something like "1 - Microsoft"
+    return `${item.value} - ${item.label}`;
+  }
+
+  // static defaultProps = {
+  //   displayTitle: true,
+  //   displayLegend: true,
+  //   legendPosition: 'right',
+  //   location: 'City'
+  // }
 
   selecionarCidades = () => {
     var dadosapi;
@@ -69,7 +144,7 @@ export default class Cidades extends Component {
     let arrayLabel = [];
     let arraySunset = [];
     let arraySunrise = [];
-    let newArr=[];
+    let newArr = [];
 
     console.log(this.state.arrayInformaAPI)
 
@@ -77,7 +152,7 @@ export default class Cidades extends Component {
 
       console.log(this.state.arrayInformaAPI.cidade[j]);
       arrayLabel.push(this.state.arrayInformaAPI.cidade[j]);
-      newArr.push(this.state.arrayInformaAPI.cidade[j],this.state.arrayInformaAPI.tempo[j])
+      newArr.push(this.state.arrayInformaAPI.cidade[j], this.state.arrayInformaAPI.tempo[j])
     }
 
 
@@ -106,7 +181,7 @@ export default class Cidades extends Component {
         graph: true,
         sunsethour: arraySunset,
         sunrisehour: arraySunrise,
-        tempoCity:newArr,
+        tempoCity: newArr,
       })
     }, 500);
 
@@ -183,35 +258,25 @@ export default class Cidades extends Component {
 
 
   OrdenarFunc = () => {
-    console.log(this.state.tempoCity)
-    // let convertar=this.state.dataGraph;
-    // convertar.sort();
-    // let novoArray,novalabel;
-    // for(let i=0;i<this.state.tempoCity.length;i++){
-      
-    //   console.log(this.state.tempoCity[i])
-    //   if(this.state.tempoCity[i]==convertar[i]){
-    //     novalabel=this.state.tempoCity[i-1];
-    //     novoArray.push(novalabel);
-        
-    //   }
-    // }
+
+
     const timer = setTimeout(() => {
       this.setState({
-        dataGraph:null,
-        //labelCidade:novoArray
+        dataGraph: null,
       });
     }, 500);
     this.mudarGraph();
   }
 
   mudarGraph = () => {
-    
+
     let arrayCerto = this.state.dataGraph;
     arrayCerto.sort();
+
+
     const timer = setTimeout(() => {
       this.setState({
-        dataGraph: arrayCerto
+        dataGraph: arrayCerto,
       });
     }, 500);
   }
@@ -238,21 +303,44 @@ export default class Cidades extends Component {
       const listar_horassunset = numbers2.map((number, index) =>
         <td key={index}>{number}</td>
       );
+
+      // const numbers3 = this.state.tempoCity;
+      // const test = numbers3.map((number, index) => {
+      //   console.log(this.state.dataGraph[index-1])
+      //   console.log(number[index-1])
+      //   if (this.state.dataGraph[index-1] === number[index]) {
+      //     console.log(number);
+      //     return (
+      //       <input key={index} type='text' name={number[index-1]} value={number[index-1]} />
+      //     );
+      //   }
+      // }
+      //);
+
       return (
         <div>
 
           <form onSubmit={this.newSubmit}>
             {/* {listItems} */}
-            <input type='text' name={'test1'} value={this.state.test1} onChange={this.onChangeInput} />
-            <input type='text' name={'test2'} value={this.state.test2} onChange={this.onChangeInput} />
-            <input type='text' name={'test3'} value={this.state.test3} onChange={this.onChangeInput} />
-            <input type='text' name={'test4'} value={this.state.test4} onChange={this.onChangeInput} />
-            <input type='text' name={'test5'} value={this.state.test5} onChange={this.onChangeInput} />
-            <input type='text' name={'test6'} value={this.state.test6} onChange={this.onChangeInput} />
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">Cidade</InputGroupAddon>
+              <Input name={'test1'} value={this.state.test1} onChange={this.onChangeInput} />
+              <InputGroupAddon addonType="prepend">Cidade</InputGroupAddon>
+              <Input name={'test2'} value={this.state.test2} onChange={this.onChangeInput} />
+              <InputGroupAddon addonType="prepend">Cidade</InputGroupAddon>
+              <Input name={'test3'} value={this.state.test3} onChange={this.onChangeInput} />
+              <InputGroupAddon addonType="prepend">Cidade</InputGroupAddon>
+              <Input name={'test4'} value={this.state.test4} onChange={this.onChangeInput} />
+              <InputGroupAddon addonType="prepend">Cidade</InputGroupAddon>
+              <Input name={'test5'} value={this.state.test5} onChange={this.onChangeInput} />
+              <InputGroupAddon addonType="prepend">Cidade</InputGroupAddon>
+              <Input name={'test6'} value={this.state.test6} onChange={this.onChangeInput} />
+            </InputGroup>
             <Button type='submit' > Pesquisar </Button>
 
           </form>
           <Button type='submit' onClick={this.OrdenarFunc} > Ordenar Temperatura </Button>
+
           <div className="chart">
             <Bar
 
@@ -301,19 +389,44 @@ export default class Cidades extends Component {
 
       return (
         <div>
+          <Autocomplete
+            getItemValue={this.getItemValue}
+            items={this.state.autocompleteData}
+            renderItem={this.renderItem}
+            value={this.state.value}
+            onChange={this.onChange}
+            onSelect={this.onSelect}
+          />
 
           <form onSubmit={this.newSubmit}>
-            {/* {listItems} */}
-            <input type='text' name={'test1'} value={this.state.test1} onChange={this.onChangeInput} />
-            <input type='text' name={'test2'} value={this.state.test2} onChange={this.onChangeInput} />
-            <input type='text' name={'test3'} value={this.state.test3} onChange={this.onChangeInput} />
-            <input type='text' name={'test4'} value={this.state.test4} onChange={this.onChangeInput} />
-            <input type='text' name={'test5'} value={this.state.test5} onChange={this.onChangeInput} />
-            <input type='text' name={'test6'} value={this.state.test6} onChange={this.onChangeInput} />
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">Cidade</InputGroupAddon>
+              <Input name={'test1'} value={this.state.test1} onChange={this.onChangeInput} />
+            </InputGroup>
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">Cidade</InputGroupAddon>
+              <Input name={'test2'} value={this.state.test2} onChange={this.onChangeInput} />
+            </InputGroup>
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">Cidade</InputGroupAddon>
+              <Input name={'test3'} value={this.state.test3} onChange={this.onChangeInput} />
+            </InputGroup>
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">Cidade</InputGroupAddon>
+              <Input name={'test4'} value={this.state.test4} onChange={this.onChangeInput} />
+            </InputGroup>
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">Cidade</InputGroupAddon>
+              <Input name={'test5'} value={this.state.test5} onChange={this.onChangeInput} />
+            </InputGroup >
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">Cidade</InputGroupAddon>
+              <Input name={'test6'} value={this.state.test6} onChange={this.onChangeInput} />
+            </InputGroup >
             <Button type='submit' > Pesquisar </Button>
 
-          </form>
-        </div>
+          </form >
+        </div >
       );
     }
 
