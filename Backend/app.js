6ce32e58d18b3e9f74dfb8b8478c11d6fef9e676
carva28 <carvalho_diogo@hotmail.com>
2019-10-15 12:57:46 +0100

    autenticação

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,8 @@ const ficheiroPedidos = require('./calls');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+
 app.use(cors())
 app.all('/', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
--- Backend/calls.js
+++ Backend/calls.js
@@ -16,33 +16,145 @@ exports.todosusers = function (req, res) {
 }
 
 exports.registo = async function (req, res) {
-    try {
+    var todos, dataTodos;
+    fs.readFile('./login.json', 'utf8', async (err, jsonString) => {
+        if (err) {
+            console.log("File read failed:", err)
+            return
+        }
+        todos = JSON.parse(jsonString)
+        //console.log(Object.keys(todos).length)
+        dataTodos = Object.keys(todos).length;
+
+
+        var arrayData = [];
+
+        var totaluser = dataTodos + 1;
+
+
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        const user = { name: req.body.name, password: hashedPassword }
-        users.push(user)
-        console.log(users)
-        res.status(201).send();
-        res.send({ message: 'utilizador inserido' })
-    } catch {
-        res.status(500).send();
-    }
+        var nome = "user" + totaluser;
+
+        var user = {};
+        user[nome] = {
+            name: req.body.name,
+            password: hashedPassword,
+        };
+        // const user = {
+        //     nome: {
+        //         name: req.body.name,
+        //         password: hashedPassword,
+        //     },
+        // }
+        // //const jsonString = JSON.stringify(user)
+        arrayData.push(todos, user);
+        console.log(arrayData);
+        const jsonString2 = JSON.stringify(arrayData, null, 2)
+        console.log(jsonString2)
+
+        fs.writeFileSync('./login.json', jsonString2, err => {
+            if (err) {
+                console.log('Error writing file', err)
+            } else {
+                console.log('Successfully wrote file')
+                res.send({ message: 'utilizador inserido' })
+            }
+        })
+    })
+
+
 }
 
 exports.login = async function (req, res) {
-    
-    const user = users.find(user => user.name === req.body.name)
-    if (user == null) {
-        return res.status(400).send('Cannot find user')
-    }
-    try {
-        if (await bcrypt.compare(req.body.password, user.password)) {
-            res.send({message:'Success'})
-        } else {
-            res.send('Not Allowed')
+    var username, user, password;
+    fs.readFile('./login.json', 'utf8', async (err, jsonString) => {
+        if (err) {
+            console.log("File read failed:", err)
+            return
         }
-    } catch {
-        res.status(500).send()
-    }
+        user = JSON.parse(jsonString)
+        //FAZER FOOR LOOPPPP
+        var dataall = Object.keys(user).length;
+        for (var i = 1; i <= dataall; i++) {
+            var nome = "user" + i;
+
+            console.log(user[nome])
+            if (user[nome].name === req.body.name) {
+                console.log(user[nome].name)
+                username = user[nome].name;
+                password = user[nome].password;
+                try {
+                    if (await bcrypt.compare(req.body.password, password)) {
+                        res.send({ message: 'Success' })
+                    } else {
+                        res.send('Not Allowed')
+                    }
+                } catch {
+                    res.status(500).send()
+                }
+                break;
+            } else {
+                if (dataall < i) {
+                    console.log('erro');
+                    username = user[nome].name;
+                    password = user[nome].password;
+                    if (username == null) {
+                        return res.status(400).send('Cannot find user')
+                    }
+                    try {
+                        if (await bcrypt.compare(req.body.password, password)) {
+                            res.send({ message: 'Success' })
+                        } else {
+                            res.send('Not Allowed')
+                        }
+                    } catch {
+                        res.status(500).send()
+                    }
+                } else if (dataall == i) {
+                    console.log('erro');
+                    username = user[nome].name;
+                    password = user[nome].password;
+                    if (username == null || username !== req.body.name) {
+                        return res.status(400).send('Cannot find user')
+                    }
+                    try {
+                        if (await bcrypt.compare(req.body.password, password)) {
+                            res.send({ message: 'Success' })
+                        } else {
+                            res.send('Not Allowed')
+                        }
+                    } catch {
+                        res.status(500).send()
+                    }
+                    break;
+                }
+
+
+            }
+        }
+
+    })
+
+
+
+
+
+
+
+
+    // const user = users.find(user => user.name === req.body.name)
+    // if (user == null) {
+    //     return res.status(400).send('Cannot find user')
+    // }
+    // try {
+    //     if (await bcrypt.compare(req.body.password, user.password)) {
+    //         res.send({ message: 'Success' })
+    //     } else {
+    //         res.send('Not Allowed')
+    //     }
+    // } catch {
+    //     res.status(500).send()
+    // }
 }
 
 
